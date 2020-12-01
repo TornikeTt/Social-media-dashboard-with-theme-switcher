92aada6121dc12047edf9f2698fffc7d6a210c50
Tornike <72967485+TornikeTt@users.noreply.github.com>
2020-12-01 10:02:55 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function Change(){
     this.body = document.getElementById("backgroundColor");
     this.circle = document.getElementById("circle")
     this.firstgridBoxes = document.querySelectorAll(".Box")
-    this.secondgridBoxes = document.querySelectorAll(".box2")
+    /*second grid boxes colors*/
+    this.secondgridBoxes_1 = document.querySelector(".facebook-Views")
+    this.secondgridBoxes_2 = document.querySelector(".facebook-Like")
+    this.secondgridBoxes_3 = document.querySelector(".instagram-Like")
+    this.secondgridBoxes_4 = document.querySelector(".instagram-Views")
+    this.secondgridBoxes_5 = document.querySelector(".Twitter-Retwwets")
+    this.secondgridBoxes_6 = document.querySelector(".Twitter-Like")
+    this.secondgridBoxes_7 = document.querySelector(".youtube-Like")
+    this.secondgridBoxes_8 = document.querySelector(".YouTube-Views")
+    /*second grid boxes colors*/
     this.TextColor = document.querySelectorAll(".c")
     this.number_TopText_Colors = document.querySelectorAll(".c2")
     this.button = document.querySelector("button")
@@ -17,7 +26,16 @@ function Change(){
     }
     this.change_boxes_background_Color = function(){
         this.firstgridBoxes.forEach(x =>  x.style.backgroundColor = "hsl(228, 28%, 20%)")
-        this.secondgridBoxes.forEach(y => y.style.backgroundColor = "hsl(228, 28%, 20%)")
+        /*background color change for second gird*/
+        this.secondgridBoxes_1.style.backgroundColor = "hsl(228, 28%, 20%)";
+        this.secondgridBoxes_2.style.backgroundColor = "hsl(228, 28%, 20%)";
+        this.secondgridBoxes_3.style.backgroundColor = "hsl(228, 28%, 20%)";
+        this.secondgridBoxes_4.style.backgroundColor = "hsl(228, 28%, 20%)";
+        this.secondgridBoxes_5.style.backgroundColor = "hsl(228, 28%, 20%)";
+        this.secondgridBoxes_6.style.backgroundColor = "hsl(228, 28%, 20%)";
+        this.secondgridBoxes_7.style.backgroundColor = "hsl(228, 28%, 20%)";
+        this.secondgridBoxes_8.style.backgroundColor = "hsl(228, 28%, 20%)";
+        /*background color change for second gird*/
     }
     this.changeTextColors = function(){
         this.TextColor.forEach(x => x.style.color = "hsl(228, 34%, 66%)");
@@ -44,7 +62,16 @@ function Changes(){
     this.body = document.getElementById("backgroundColor");
     this.circle = document.getElementById("circle")
     this.firstgridBoxes = document.querySelectorAll(".Box")
-    this.secondgridBoxes = document.querySelectorAll(".box2")
+    /*second grid boxes colors*/
+    this.secondgridBoxes_1 = document.querySelector(".facebook-Views")
+    this.secondgridBoxes_2 = document.querySelector(".facebook-Like")
+    this.secondgridBoxes_3 = document.querySelector(".instagram-Like")
+    this.secondgridBoxes_4 = document.querySelector(".instagram-Views")
+    this.secondgridBoxes_5 = document.querySelector(".Twitter-Retwwets")
+    this.secondgridBoxes_6 = document.querySelector(".Twitter-Like")
+    this.secondgridBoxes_7 = document.querySelector(".youtube-Like")
+    this.secondgridBoxes_8 = document.querySelector(".YouTube-Views")
+    /*second grid boxes colors*/
     this.TextColor = document.querySelectorAll(".c")
     this.number_TopText_Colors = document.querySelectorAll(".c2")
     this.button = document.querySelector("button")
@@ -59,7 +86,14 @@ function Changes(){
     }
     this.change_boxes_background_Color = function(){
         this.firstgridBoxes.forEach(x =>  x.style.backgroundColor = "hsl(227, 47%, 96%)")
-        this.secondgridBoxes.forEach(y => y.style.backgroundColor = "hsl(227, 47%, 96%)")
+        this.secondgridBoxes_1.style.backgroundColor = "hsl(227, 47%, 96%)";
+        this.secondgridBoxes_2.style.backgroundColor = "hsl(227, 47%, 96%)";
+        this.secondgridBoxes_3.style.backgroundColor = "hsl(227, 47%, 96%)";
+        this.secondgridBoxes_4.style.backgroundColor = "hsl(227, 47%, 96%)";
+        this.secondgridBoxes_5.style.backgroundColor = "hsl(227, 47%, 96%)";
+        this.secondgridBoxes_6.style.backgroundColor = "hsl(227, 47%, 96%)";
+        this.secondgridBoxes_7.style.backgroundColor = "hsl(227, 47%, 96%)";
+        this.secondgridBoxes_8.style.backgroundColor = "hsl(227, 47%, 96%)";
     }
     this.changeTextColors = function(){
         this.TextColor.forEach(x => x.style.color = "hsl(228, 12%, 44%)");
